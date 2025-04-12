b23f41cfd2dbc5f8db1c2dd0bca79f84dbb24c0a
Vinh Dang <vinh.dang@se.com>
2025-04-12 11:58:55 -0700

    Update from work

---
+++ nvim/after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+local set = vim.opt_local
+
+set.shiftwidth = 2
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
--- nvim/lua/qvd808/plugins/lsp.lua
+++ nvim/lua/qvd808/plugins/lsp.lua
@@ -5,15 +5,15 @@ return {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
-      { 'folke/neodev.nvim',       opts = {} },  -- Use for configuring Lua LSP
+      { 'folke/neodev.nvim',       opts = {} },     -- Use for configuring Lua LSP
     },
     config = function()
       -- List of servers to install and configure
       local servers = {
-        lua_ls = {}, -- Lua
+        -- lua_ls = {}, -- Lua
         pyright = {}, -- Python
-        clangd = {}, -- C/C++
-        ts_ls = {}, -- Typescript/Javascript
+        clangd = {},  -- C/C++
+        -- ts_ls = {}, -- Typescript/Javascript
       }
 
       -- Setup capabilities
@@ -36,8 +36,7 @@ return {
       -- Adding LSP option on LspAttach
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+          local client = vim.lsp.get_client_by_id(args.data.client_id) if not client then return end
 
           -- Formatting on save
           if client.supports_method("textDocument/formatting") then
@@ -51,11 +50,14 @@ return {
 
           -- Diagnostic keymaps
           vim.keymap.set("n", "<leader>e", vim.diagnostic.setqflist, { desc = "Show diagnostic [E]rror message" })
+          vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror message" })
           vim.keymap.set("n", "grn", vim.lsp.buf.rename)
           vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
           vim.keymap.set("n", "grr", vim.lsp.buf.references)
           vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
           vim.keymap.set("n", 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+          vim.keymap.set("n", 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation'})
+          vim.keymap.set("n", 'gd', require('telescope.builtin').lsp_definitions, {desc = 'Go to Definition'})
         end,
       })
     end

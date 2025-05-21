7c1108f05b360ab96813d2c0b0552d313fa6f1df
Vinh Dang <vinh.dang@se.com>
2025-05-21 14:26:25 -0700

    Template

--- nvim/lua/qvd808/plugins/lsp.lua
+++ nvim/lua/qvd808/plugins/lsp.lua
@@ -10,12 +10,12 @@ return {
     config = function()
       -- List of servers to install and configure
       local servers = {
-        lua_ls = {},      -- Lua
-        pyright = {},     -- Python
-        clangd = {},      -- C/C++
-        ts_ls = {},       -- Typescript/Javascript
-        tailwindcss = {}, --Tailwind
-        zls = {},         -- Zig
+        lua_ls = {},  -- Lua
+        pyright = {}, -- Python
+        clangd = {},  -- C/C++
+        -- ts_ls = {},       -- Typescript/Javascript
+        -- tailwindcss = {}, --Tailwind
+        -- zls = {},         -- Zig
         -- arduino_language_server = {}, -- Arduino
       }
 
@@ -39,7 +39,66 @@ return {
       -- Adding LSP option on LspAttach
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id) if not client then return end
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          -- Detect if there is multiple compile_commands.json for clangd clients
+          if client.name == "clangd" then
+            vim.api.nvim_buf_create_user_command(args.buf, "SearchCompileCommands", function()
+              local has_fd = vim.fn.executable("fd") == 1
+              local find_command = has_fd
+                  and { "fd", "--hidden", "--no-ignore", "compile_commands.json" }
+                  or { "find", ".", "-name", "compile_commands.json" }
+
+              -- Run the command and capture output
+              local handle = io.popen(table.concat(find_command, " "))
+              if not handle then
+                vim.notify("Failed to run find command", vim.log.levels.ERROR)
+                return
+              end
+
+              local result = handle:read("*a")
+              handle:close()
+
+              local files = {}
+              for line in result:gmatch("[^\r\n]+") do
+                table.insert(files, line)
+              end
+
+              if #files == 0 then
+                vim.notify("No compile_commands.json files found", vim.log.levels.WARN)
+                return
+              elseif #files == 1 then
+                vim.notify("Using: " .. files[1])
+                vim.lsp.stop_client(client.id)
+                vim.defer_fn(function()
+                  require("lspconfig").clangd.setup({
+                    cmd = { "clangd", "--compile-commands-dir=" .. vim.fn.fnamemodify(files[1], ":h") }
+                  })
+                  vim.cmd("edit") -- reload buffer
+                end, 100)
+              else
+                vim.ui.select(files, {
+                  prompt = "Select compile_commands.json for clangd",
+                  format_item = function(item)
+                    return vim.fn.fnamemodify(item, ":.")
+                  end,
+                }, function(choice)
+                  if choice then
+                    vim.notify("Using: " .. choice)
+                    vim.lsp.stop_client(client.id)
+                    vim.defer_fn(function()
+                      require("lspconfig").clangd.setup({
+                        cmd = { "clangd", "--compile-commands-dir=" .. vim.fn.fnamemodify(choice, ":h") }
+                      })
+                      vim.cmd("edit") -- reload buffer
+                    end, 100)
+                  end
+                end)
+              end
+            end, { desc = "Search for compile_commands.json" })
+          end
+
 
           -- Formatting on save
           if client.supports_method("textDocument/formatting") then
@@ -59,8 +118,9 @@ return {
           vim.keymap.set("n", "grr", vim.lsp.buf.references)
           vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
           vim.keymap.set("n", 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
-          vim.keymap.set("n", 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation'})
-          vim.keymap.set("n", 'gd', require('telescope.builtin').lsp_definitions, {desc = 'Go to Definition'})
+          vim.keymap.set("n", 'gI', require('telescope.builtin').lsp_implementations,
+            { desc = '[G]oto [I]mplementation' })
+          vim.keymap.set("n", 'gd', require('telescope.builtin').lsp_definitions, { desc = 'Go to Definition' })
         end,
       })
     end

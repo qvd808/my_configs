4cbdbbd07931bcf4164366232f94f872aeedaf43
Vinh Dang <vinh.dang@se.com>
2025-06-12 10:30:51 -0700

    Adding debug adapter protocol

--- nvim/lua/qvd808/plugins/lsp.lua
+++ nvim/lua/qvd808/plugins/lsp.lua
@@ -26,7 +26,7 @@ return {
       -- Setup Mason
       require("mason-lspconfig").setup({
         ensure_installed = vim.tbl_keys(servers), -- Install listed servers
-        automatic_installation = true
+        automatic_enable = false
       })
 
       -- Setup LSP servers
@@ -39,7 +39,63 @@ return {
       -- Adding LSP option on LspAttach
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id) if not client then return end
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then return end
+
+          --     -- Detect if there is multiple compile_commands.json for clangd clients
+          --     if client.name == "clangd" then
+          --       vim.api.nvim_buf_create_user_command(args.buf, "SearchCompileCommands", function()
+          --         local has_fd = vim.fn.executable("fd") == 1
+          --         local find_command = has_fd
+          --             and { "fd", "--hidden", "--no-ignore", "compile_commands.json" }
+          --             or { "find", ".", "-name", "compile_commands.json" }
+          --
+          --         -- Run the command and capture output
+          --         local handle = io.popen(table.concat(find_command, " "))
+          --         if not handle then
+          --           vim.notify("Failed to run find command", vim.log.levels.ERROR)
+          --           return
+          --         end
+          --
+          --         local result = handle:read("*a")
+          --         handle:close()
+          --
+          --         local files = {}
+          --         for line in result:gmatch("[^\r\n]+") do
+          --           table.insert(files, line)
+          --         end
+          --
+          --         if #files == 0 then
+          --           vim.notify("No compile_commands.json files found", vim.log.levels.WARN)
+          --           return
+          --         elseif #files == 1 then
+          --           local dir = vim.fn.fnamemodify(files[1], ":h")
+          --           vim.notify("Using: " .. files[1])
+          --           client.config.cmd = { "clangd", "--compile-commands-dir=" .. dir }
+          --           vim.lsp.stop_client(client.id)
+          --           vim.defer_fn(function()
+          --             vim.cmd("edit") -- reload buffer to trigger LspAttach
+          --           end, 100)
+          --         else
+          --           vim.ui.select(files, {
+          --             prompt = "Select compile_commands.json for clangd",
+          --             format_item = function(item)
+          --               return vim.fn.fnamemodify(item, ":.")
+          --             end,
+          --           }, function(choice)
+          --             if choice then
+          --               local dir = vim.fn.fnamemodify(choice, ":h")
+          --               vim.notify("Using: " .. choice)
+          --               client.config.cmd = { "clangd", "--compile-commands-dir=" .. dir }
+          --               vim.lsp.stop_client(client.id)
+          --               vim.defer_fn(function()
+          --                 vim.cmd("edit") -- reload buffer to trigger LspAttach
+          --               end, 100)
+          --             end
+          --           end)
+          --         end
+          --       end, { desc = "Search for compile_commands.json" })
+          --     end
 
           -- Formatting on save
           if client.supports_method("textDocument/formatting") then
---
+++ nvim/lua/qvd808/plugins/nvim-dap.lua
@@ -0,0 +1,90 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    lazy = true,
+    -- Copied from LazyVim/lua/lazyvim/plugins/extras/dap/core.lua and
+    -- modified.
+    keys = {
+      {
+        "<leader>b",
+        function() require("dap").toggle_breakpoint() end,
+        desc = "Toggle Breakpoint"
+      },
+
+      {
+        "<leader>cc",
+        function() require("dap").continue() end,
+        desc = "Continue"
+      },
+
+      {
+        "<leader>dC",
+        function() require("dap").run_to_cursor() end,
+        desc = "Run to Cursor"
+      },
+
+      {
+        "<leader>dT",
+        function() require("dap").terminate() end,
+        desc = "Terminate"
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    ---@type MasonNvimDapSettings
+    opts = {
+      -- This line is essential to making automatic installation work
+      -- :exploding-brain
+      handlers = {},
+      automatic_installation = false,
+      -- DAP servers: Mason will be invoked to install these if necessary.
+      ensure_installed = {
+        "python",
+      },
+    },
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "williamboman/mason.nvim",
+    },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    lazy = true,
+    config = function()
+      local python = vim.fn.expand("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+      require("dap-python").setup(python)
+    end,
+    -- Consider the mappings at
+    -- https://github.com/mfussenegger/nvim-dap-python?tab=readme-ov-file#mappings
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = true,
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = true,
+    keys = {
+      {
+        "<leader>du",
+        function()
+          require("dapui").toggle({})
+        end,
+        desc = "Dap UI"
+      },
+    },
+    dependencies = {
+      "jay-babu/mason-nvim-dap.nvim",
+      "mfussenegger/nvim-dap-python",
+      "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+  }
+}

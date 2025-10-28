6a6c1525b7c2629d46bcdaa757d0711e999a1196
Quang Vinh Dang <dqvinh101@gmail.com>
2025-10-27 21:49:30 -0700

    Adding prettier for other file and get the config file

--- nvim/lua/qvd808/plugins/conform.lua
+++ nvim/lua/qvd808/plugins/conform.lua
@@ -2,18 +2,26 @@ return {
   "stevearc/conform.nvim",
   opts = {},
   config = function()
+    local project_root = vim.fn.getcwd() -- adjust if you want dynamic root detection
+
     require("conform").setup({
       format_on_save = {
         timeout_ms = 3000,
-        lsp_format = "fallback", -- use LSP if no formatter configured
+        lsp_format = "fallback",
       },
+      async = true,
       formatters_by_ft = {
         c = { "clang-format" },
         cpp = { "clang-format" },
         lua = { "stylua" },
         javascript = { "prettier" },
         typescript = { "prettier" },
+        json = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
         rust = { "rustfmt" },
+        toml = { "taplo" },
+        sh = { "shfmt" },
       },
       formatters = {
         ["clang-format"] = {
@@ -21,7 +29,24 @@ return {
         },
         ["rustfmt"] = {
           command = "rustfmt",
-          args = { "--edition", "2021" },
+          args = { "--edition=2021" },
+        },
+        ["taplo"] = {
+          command = "taplo",
+          args = { "format", "-" },
+        },
+        ["shfmt"] = {
+          command = "shfmt",
+          args = { "-i", "2", "-bn", "-ci" },
+        },
+        ["prettier"] = {
+          command = "prettier",
+          args = {
+            "--config",
+            project_root .. "/.prettierrc", -- always use root config
+            "--stdin-filepath",
+            "$FILENAME",
+          },
         },
       },
     })
